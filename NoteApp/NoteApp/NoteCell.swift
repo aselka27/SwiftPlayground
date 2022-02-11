bbfbc15098f9ea9adfba165b94b6874ba23de203
aselka27 <90374492+aselka27@users.noreply.github.com>
2022-02-11 19:30:41 +0100

    Pushed to do list app

---
+++ NoteApp/NoteApp/NoteCell.swift
@@ -0,0 +1,14 @@
+//
+//  NoteCell.swift
+//  NoteApp
+//
+//  Created by саргашкаева on 11.02.2022.
+//
+
+import UIKit
+class NoteCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var descLabel: UILabel!
+}
